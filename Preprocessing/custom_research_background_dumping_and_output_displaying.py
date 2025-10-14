0f59fddf65ba4cc0a6721da007d4815869f19b77
Nikita Kazeev <kazeevn@gmail.com>
2025-10-14 17:43:06 +0800

    Update research background content and increment experiment version to v0.3

--- Preprocessing/custom_research_background_dumping_and_output_displaying.py
+++ Preprocessing/custom_research_background_dumping_and_output_displaying.py
@@ -5,13 +5,21 @@ import argparse
 
 def research_background_to_json(research_background_file_path):
     # YOUR RESEARCH QUESTION HERE
-    research_question = '''
-    Design a generative model for materials that will produce stable, unique, novel, and realistic crystalline materials.
-    '''
+    research_question = \
+'''
+How can we build a generative model based on Wyckoff positions that will faithfully reproduce the distribution of symmetry space groups and outperform the SOTA models in terms of stability, uniqueness, and novelty?
+'''
 
     # YOUR BACKGROUND SURVEY HERE
-    background_survey = ''
-
+    background_survey = \
+'''
+Background survey:
+1. SOTA diffusion models for solid state materials produce an abnormally high fraction of materials lacking symmetry. In the MP-20 dataset more than 98% of materials  have internal symmetry, while only ~70% produced by DiffCSP & FlowMM do.
+2. Most atoms in stable materials occupy special Wyckoff positions, allowing encoding the crystal structure based on Wyckoff positions.
+3. Wyckoff positions are discrete, and can be represented as categorical variables.
+4. Existing generative models based on Wyckoff positions underperform the SOTA models in terms of stability, uniqueness, and novelty.
+5. Existing generative models based on Wyckoff positions build their representation using Wyckoff letters and don't take into account the possible existence of multiple equvalent Wyckoff representations for the same crystal structure.
+'''
 
     # Save the research question and background survey to a JSON file
     with open(research_background_file_path, "w", encoding="utf-8") as f:
--- main.sh
+++ main.sh
@@ -53,7 +53,7 @@ model_name_insp_retrieval="gpt-4.1-2025-04-14"
 model_name_gene="gpt-4.1-2025-04-14"
 model_name_eval="gpt-4.1-2025-04-14"
 
-experiment_name="wyformer_v0.2"
+experiment_name="wyformer_v0.3"
 checkpoint_root_dir="./Checkpoints/"${experiment_name}
 mkdir -p ${checkpoint_root_dir}
 display_txt_file_path="hypothesis/"${experiment_name}".txt"
